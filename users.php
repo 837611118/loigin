9c9a54cdbf7c3ad5c33c5f9687e165df812fbafc
837611118 <144031540+837611118@users.noreply.github.com>
2024-03-08 13:47:25 +0100

    Add files via upload

---
+++ users.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit;
+}
+
+$user_id = $_SESSION['user_id'];
+$username = $_SESSION['username'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Welcome</title>
+</head>
+<body>
+    
+    <h2>welkom <?php echo $username; ?>.</h2>
+   <br>
+    <?php if ($username === 'bart') : ?>
+        <a href="change_password.php">Change Password</a>
+    <?php endif; ?> <br><br>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
